d025a45ab89c23d81c941ba3a43360f7def0c85f
response5775 <kushnerdavid90+gg@gmail.com>
2025-01-28 22:05:17 +0545

    Update requirements_installer.py

--- Swapsway redeemer - free/SourceCode/Modules/requirements_installer.py
+++ Swapsway redeemer - free/SourceCode/Modules/requirements_installer.py
@@ -1,1 +1,31 @@
+import os
+
+csa = 'py -m pip install' # change if it doesnt work
+
+def exec(command):
+  os.system(command=command)
+
+def install_requirements():
+  os.system("cls")
+  try:
+    import tls_client
+    import yaml
+    import colorama
+    import requests
+    import pyfiglet
+    import termcolor
+    import typing_extensions
+    import ctypes
+    import licensing
+  except:
+    exec(f'{csa} tls_client')
+    exec(f'{csa} pyyaml')
+    exec(f'{csa} colorama')
+    exec(f'{csa} requests')
+    exec(f'{csa} pyfiglet')
+    exec(f'{csa} termcolor')
+    #exec(f'{csa} concurrent.futures')
+    exec(f'{csa} typing_extentions')
+    exec(f'{csa} ctypes')
+    exec(f'{csa} licensing')
 
